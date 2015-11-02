93a3b648b5e1b0cbabbfe420fc627b62459861e2
sohjiro <felipe@makingdevs.com>
2015-11-01 23:44:52 -0600

    adding elements for watching videos

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, only: [:show]
   end
 
   scope "/manage", Rumbl do
