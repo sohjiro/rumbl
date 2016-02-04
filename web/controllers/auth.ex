ec14b24b05998a8bf9701b0cfee043a7244e607f
sohjiro <felipe@makingdevs.com>
2016-02-03 18:20:57 -0600

    adding annotations and slug and i don't know what else... yeah i miss some commits before :(

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -13,6 +13,9 @@ defmodule Rumbl.Auth do
   end
 
   def login(conn, user) do
+    token = Phoenix.Token.sign(conn, "tumamaesminovia", user.id)
+    IO.inspec token
+
     conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
