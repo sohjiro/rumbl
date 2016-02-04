fc81910cffb8ebc70b5bb0500c72bf0810d796be
sohjiro <felipe@makingdevs.com>
2016-02-04 17:13:57 -0600

    giving format and seeking time

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -28,11 +28,57 @@ let Video = {
       this.renderAnnotation(msgContainer, resp);
     });
 
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+                    e.target.parentNode.getAttribute("data-seek")
+      if(!seconds) { return }
+
+      Player.seekTo(seconds)
+    });
+
     vidChannel.join()
-      .receive("ok", ({annotations}) => {
-        annotations.forEach( ann => this.renderAnnotation(msgContainer, ann))
+      .receive("ok", (resp) => {
+        this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("join failed", resp));
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){
+    let template = document.createElement("div")
+    template.innerHTML = `
+    <a href="#" data-seek="${at}">
+      [${this.formatTime(at)}]
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations) {
+    setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer) {
+    return annotations.filter( ann => {
+      if(ann.at > seconds) {
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at) {
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
   },
 
   esc(str) {
@@ -41,16 +87,6 @@ let Video = {
     return div.innerHTML;
   },
 
-  renderAnnotation(msgContainer, {user, body, at}){
-    let template = document.createElement("div")
-    template.innerHTML = `
-    <a href="#" data-seek="${at}">
-      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
-    </a>
-    `
-    msgContainer.appendChild(template)
-    msgContainer.scrollTop = msgContainer.scrollHeight
-  }
 }
 
 export default Video
